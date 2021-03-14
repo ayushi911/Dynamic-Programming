125f318e8dff177aa74f31d8c878a2d81f35ff1d
Ayushi Verma <66084792+ayushi911@users.noreply.github.com>
2021-03-14 08:52:10 +0530

    Update 01knapsacktopdown.cpp

--- 0-1 Knapsack/01knapsacktopdown.cpp
+++ 0-1 Knapsack/01knapsacktopdown.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-//#include<cstring>
 using namespace std;
 
 int knapsack(int val[], int wt[], int W, int n){
@@ -14,9 +13,9 @@ int knapsack(int val[], int wt[], int W, int n){
      for(int i=1;i<n+1;++i){
         for(int j=1 ; j<W+1 ; ++j){
             if(wt[i-1]<=j)
-                t[i][j] = max(val[i-1]+knapsack(val,wt,W-wt[i-1],n-1), knapsack(val,wt,W,n-1));
-            else if (wt[n-1]> W)
-                t[i][j] = knapsack(val,wt,W,n-1);
+                t[i][j] = max(val[i-1]+t[i-1][j-1], t[i-1][j]);
+            else if (wt[i-1]> j)
+                t[i][j] = t[i-1][j];
         }     
     }
 return t[n][W];
@@ -28,13 +27,6 @@ int main(){
     int wt[]={9,10,2,1};
     int W=10, n=4;
     int j= knapsack(val,wt,W,n);
-    // for(int i=0;i<n+2;++i){
-    //     for(int j=0;j<W+2;++j){
-    //         cout<<t[i][j]<<" ";
-
-    //     }
-    //     cout<<endl;
-    // }
     cout<<j;
     return 0;
 } 
