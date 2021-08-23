86af08213a3b380c4f1e956eb79e500a0bcad5dc
Ayushi Verma <66084792+ayushi911@users.noreply.github.com>
2021-08-23 17:35:59 +0530

    Update 01knapsacktopdown.cpp

--- 0-1 Knapsack/01knapsacktopdown.cpp
+++ 0-1 Knapsack/01knapsacktopdown.cpp
@@ -13,7 +13,7 @@ int knapsack(int val[], int wt[], int W, int n){
      for(int i=1;i<n+1;++i){
         for(int j=1 ; j<W+1 ; ++j){
             if(wt[i-1]<=j)
-                t[i][j] = max(val[i-1]+t[i-1][j-1], t[i-1][j]);
+                t[i][j] = max(val[i-1]+t[i-1][j-wt[i-1]], t[i-1][j]);
             else if (wt[i-1]> j)
                 t[i][j] = t[i-1][j];
         }     
