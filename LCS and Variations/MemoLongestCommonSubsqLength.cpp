c83bd9117cc4d2bf34d7a796ed97551445c8d048
Ayushi Verma <66084792+ayushi911@users.noreply.github.com>
2021-10-05 15:14:13 +0530

    Update MemoLongestCommonSubsqLength.cpp

--- LCS and Variations/MemoLongestCommonSubsqLength.cpp
+++ LCS and Variations/MemoLongestCommonSubsqLength.cpp
@@ -1,13 +1,16 @@
 #include<bits/stdc++.h>
 using nampespace std;
+
 int static dp[1001][1001];
 int lcs(string x, string y, int n, int m){
+  if (m == 0 || n == 0)
+        return 0;
   if(dp[n][m]!=-1)
      return dp[n][m];
   if(x[n-1]==y[m-1])
     return dp[n][m] = 1+lcs(x,y,n-1,m-1);
   else
-    return dp[n][m]=max(lcs(x,y,n,m-1),lcs(x,y,n-1,m));
+    return dp[n][m]=max(lcs(x,y,n-1,m),lcs(x,y,n,m-1));
 }
 int main(){
   string x,y;
