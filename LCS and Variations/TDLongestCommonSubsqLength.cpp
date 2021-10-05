25e9a32c5f060b48841a3049f8ef5df1f8b0b0e1
Ayushi Verma <66084792+ayushi911@users.noreply.github.com>
2021-10-05 15:13:49 +0530

    Create TDLongestCommonSubsqLength.cpp

---
+++ LCS and Variations/TDLongestCommonSubsqLength.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int static dp[1001][1001];
+int lcs(string x, string y, int n, int m){
+  if (m == 0 || n == 0)
+        return 0;
+  if(dp[n][m]!=-1)
+     return dp[n][m];
+  if(x[n-1]==y[m-1])
+    return dp[n][m] = 1+lcs(x,y,n-1,m-1);
+  else
+    return dp[n][m]=max(lcs(x,y,n-1,m),lcs(x,y,n,m-1));
+}
+int main(){
+  string x,y;
+  cin>>x;
+  cin>>y;
+  int n=x.length();
+  int m=y.length();
+  memset(dp,-1,sizeof(dp));
+  cout<<lcs(x,y,n,m)<<endl;
+  return 0;
+}
