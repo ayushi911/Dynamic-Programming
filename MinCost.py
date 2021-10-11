63e2bf3571336668a65e594a8f8d81b50d79a693
Ayushi Verma <66084792+ayushi911@users.noreply.github.com>
2021-10-11 14:10:41 +0530

    Create MinCost.py

---
+++ MinCost.py
@@ -0,0 +1,12 @@
+def mincost(arr,i,j):
+  if i==j:
+    return 0
+  mini = 10**9
+  for k in range(i,j):
+    temp = mincost(arr,i,k)+mincost(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j])
+    if temp<mini:
+      mini = temp
+  return mini
+
+arr = [1, 2, 3, 4, 3]
+print(mincost(arr,0,len(arr)-1))
